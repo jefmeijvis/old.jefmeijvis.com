b769162953697bae2a2ed65a401ef433aa90f235
Jef Meijvis <jef.meijvis@hotmail.com>
2023-01-23 13:41:14 +0100

    Spelling mistakes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,19 +6,14 @@
 <h1>About</h1>
 <p>
     My name is Jef. I'm a software engineer, focusing mostly on the C# / Dotnet / Azure ecosystem. 
-    I work at as a software consultant at <a rel=noreferrer href="https://www.dotnetlab.eu">DotnetLab</a>, a company focussing on Microsoft technologies.
+    I work as a software consultant at <a rel=noreferrer href="https://www.dotnetlab.eu">DotnetLab</a>, a company focussing on Microsoft technologies.
     This blog will be used to post anything related to Dotnet development and Azure!</p>
   <p>
     Recently I've been awarded the Microsoft Certified Trainer title!
   </p>
+
   <h2>The website</h2>
-  <p>The site was made using <a rel=noreferrer href="https://svelte.dev/">Svelte</a> and <a rel=noreferrer href="https://kit.svelte.dev/">Sveltekit</a>. 
-  Originally I planned on using a headless CMS such as Strapi, but ended up with including my blogging articles in the frontend repo as well.
-  While this isn't ideal, it recudes the scope of this website to a single frontend app running on a Node server. 
-  It also allows me to include some custom elements inside a blogpost, if needed.</p>
-  <p>Feel free to <a rel=noreferrer href="/contact">contact me!</a></p>
-
-
+  <p>If you want more information about how the website is build, there is <a href="/blog/009-building-the-blog">a blogpost</a> that describes the entire stack of how this site is build.</p>
 
   <style>
       p
