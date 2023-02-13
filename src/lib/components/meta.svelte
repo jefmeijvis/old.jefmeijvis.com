5e07f54ea8a90363c5f605535fe06f67ebd656b1
Jef Meijvis <jef.meijvis@hotmail.com>
2023-02-14 00:07:58 +0100

    Updated OG description

--- src/lib/components/meta.svelte
+++ src/lib/components/meta.svelte
@@ -20,13 +20,13 @@
     {#if !$page.url.pathname.includes("/blog/")}
         <meta name="image" property="og:image" content="/opengraph.png">
         <meta name="twitter:image" content="/opengraph.png">
-        <meta name="twitter:title" content="Software blogging about Microsoft Azure and .NET">
-        <meta name="twitter:description" content="Software blogging about Microsoft Azure and .NET">
+        <meta name="twitter:title" content="Software blogging about MSvelte, Security, Microsoft Azure and .NET">
+        <meta name="twitter:description" content="Software blogging about Svelte, Security, Microsoft Azure and .NET">
     {/if}
-    <meta name="description" content="Software blogging about Microsoft Azure and .NET">
+    <meta name="description" content="Software blogging about Svelte, Security, Microsoft Azure and .NET">
     <meta property="og:site_name" content="Jef Meijvis" />
-    <meta property=“og:title” content="Software blogging about Microsoft Azure and .NET" />
-    <meta property="og:description" content="Software blogging about Microsoft Azure and .NET" />
+    <meta property=“og:title” content="Software blogging about Svelte, Security, Microsoft Azure and .NET" />
+    <meta property="og:description" content="Software blogging about Svelte, Security, Microsoft Azure and .NET" />
     <meta property="og:url" content="https://www.jefmeijvis.com" />
     <meta property="og:type" content="article" />
     <meta property="article:publisher" content="https://www.jefmeijvis.com/about" />
