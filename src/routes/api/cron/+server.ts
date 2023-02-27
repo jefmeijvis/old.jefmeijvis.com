6b060251eae3477b890c8fa340beccbe2311a424
Jef Meijvis <jef.meijvis@hotmail.com>
2023-02-27 20:56:26 +0100

    Cron test

--- src/routes/api/cron/+server.ts
+++ src/routes/api/cron/+server.ts
@@ -4,5 +4,6 @@
 
 export function GET(req : any) {
     console.log("This is a log line with timestamp "+ new Date().toISOString());
+    console.log(process.env.VERCEL_DEPLOYMENT_URL)
     return new Response('Hello Cron!');
   }
