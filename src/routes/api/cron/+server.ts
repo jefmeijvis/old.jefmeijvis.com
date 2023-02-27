790516d550af75b9742588a871b47ed6c0dba6f9
Jef Meijvis <jef.meijvis@hotmail.com>
2023-02-27 21:17:34 +0100

    CRON update

--- src/routes/api/cron/+server.ts
+++ src/routes/api/cron/+server.ts
@@ -1,6 +1,16 @@
 export async function GET(req : any) 
 {
-    let res = await fetch(process.env.VERCEL_DEPLOYMENT_URL ?? "", {method : 'GET'});
+  console.dir(process.env)
+    let url : string | undefined = process.env.VERCEL_URL;
+
+    if(!url)
+    {
+      console.log("Unable to get VERCEL_URL");
+      return new Response('Unable to get VERCEL_URL');
+    }
+
+    let res = await fetch( url, {method : 'GET'});
     let jsonResponse = await res.json();
+    console.log(jsonResponse);
     return new Response(jsonResponse);
   }
