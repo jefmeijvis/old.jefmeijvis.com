446c875b432a4f4ca5fd5d5e339d94f23fdd0ebf
Jef Meijvis <jef.meijvis@hotmail.com>
2023-05-24 11:26:14 +0200

    Revert "OG Fix"
    This reverts commit 6f07aca96e2f2adf6b4d3bbb0e590d074ea50f40.

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -11,9 +11,7 @@
     export let data: { id : number, slug : string , post : Post, timestamp : string};
 
     let source = data.post.markdown;
-    let og; string = "";
-    if(data && data.post && data.post.filename)
-        og = "https://www.jefmeijvis.com/post/" + data.post.filename.slice(0,3) + "/opengraph.png";
+    let og : string = "https://www.jefmeijvis.com/post/" + data.post.filename.slice(0,3) + "/opengraph.png";
 
     // Reload the markdown from the pageload data
     afterNavigate(() => DoAfterNavigate());
