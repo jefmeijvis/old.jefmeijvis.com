b9f58975b6d7da21da50059d085cf74ff2c64259
Jef Meijvis <jef.meijvis@hotmail.com>
2023-09-14 08:39:02 +0200

    Identified Flipboard RSS Crawler

--- src/lib/ts/vendor.ts
+++ src/lib/ts/vendor.ts
@@ -25,7 +25,12 @@
         } else if (userAgent.includes("Safari")) {
             // "Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1"
             return "Apple Safari";
-        } else {
+        } else if (userAgent.includes("FlipboardRSS")) 
+        {
+            // Mozilla/5.0 (compatible; FlipboardRSS/1.2; +http://flipboard.com/browserproxy)
+            return "Flipboard RSS Crawler"
+        }
+        else {
             return "unknown";
         }
     }
