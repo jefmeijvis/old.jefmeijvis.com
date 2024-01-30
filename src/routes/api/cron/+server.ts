c54da4748084b6bbd8cc0c5414692d6aa3a21039
Jef Meijvis <jef.meijvis@hotmail.com>
2024-01-30 21:56:08 +0100

    Added cornucopia trigger

--- src/routes/api/cron/+server.ts
+++ src/routes/api/cron/+server.ts
@@ -2,14 +2,21 @@ export async function GET(req : any)
 {
     let url : string | undefined = process.env.VERCEL_HOOK;
 
-    if(!url)
+    let cornucopiaUrl : string | undefined = process.env.VERCEL_CORNUCOPIA_HOOK;
+
+
+    if(!url || !cornucopiaUrl)
     {
-      console.log("Unable to get VERCEL_HOOK");
-      return new Response('Unable to get VERCEL_HOOK');
+      console.log("Unable to get VERCEL_HOOK or VERCEL_CORNUCOPIA_HOOK");
+      return new Response('Unable to get VERCEL_HOOK or VERCEL_CORNUCOPIA_HOOK');
     }
 
     let res = await fetch( url, {method : 'GET'});
     let jsonResponse = await res.json();
     console.log(jsonResponse);
+
+    let res2 = await fetch( cornucopiaUrl, {method : 'GET'});
+    let jsonResponse2 = await res.json();
+    console.log(jsonResponse2);
     return new Response(jsonResponse);
   }
